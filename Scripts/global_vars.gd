b6a4ef371cfbc2ca63e20df16f8e4c49d9ed898e
Jhaz <jhazmineaudrey7@gmail.com>
2026-05-03 21:17:30 +0800

    feat: add main menu and global vars script

---
+++ Scripts/global_vars.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var fish_unlock : bool = false
+var salmon_unlock : bool = false
+var vsquid_unlock : bool = false
+var axolotl_unlock : bool = false
+var whale_unlock : bool = false
+var pearl_unlock : bool = false
+
+var fish_qty : int = 0
+var salmon_qty : int = 0
+var vsquid_qty : int = 0
+var axolotl_qty : int = 0
+var whale_qty : int = 0
+var pearl_qty : int = 0
+
+var possible_cat_wants = ["Fish", "Salmon", "VSquid", "Axolotl", "Whale", "Pearl"]
+
+var current_cat_want : String
