1780477ece0577b2c7d351218eec4e5254dc6ba8
Jhaz <jhazmineaudrey7@gmail.com>
2026-05-04 14:29:43 +0800

    feat: change close backpack function

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -32,8 +32,6 @@ extends Control
 @onready var pearl_pic: Sprite2D = $BackpackItems/PearlPic
 
 @onready var backpack_toggle: TextureButton = $BackpackToggle
-@onready var close_backpack_area: ColorRect = $CloseBackpackArea
-var hovering_outside = false
 
 func _ready() -> void:
 	normal_bg.visible = true
@@ -41,35 +39,22 @@ func _ready() -> void:
 	cats.position = Vector2(0, 0)
 	backpack_items.position.x = -570.555
 	backpack_toggle.position = Vector2(0, 0)
-	close_backpack_area.mouse_filter = Control.MOUSE_FILTER_IGNORE
-	
-func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("Click"):
-		if hovering_outside == true:
-			backpack_hide()
-			set_process_input(false)
 	
 func backpack_show():
 	create_tween().tween_property(backpack_items, "position:x", 0, 0.5).set_ease(Tween.EASE_IN_OUT)
 	create_tween().tween_property(cats, "position:x", 0, 0.5).set_ease(Tween.EASE_IN_OUT) 
 	create_tween().tween_property(cats, "position:x", 218.275, 0.5).set_ease(Tween.EASE_IN_OUT) 
 	backpack_toggle.disabled = true
-	close_backpack_area.mouse_filter = Control.MOUSE_FILTER_PASS
-	set_process_input(true)
 	
 func backpack_hide():
 	create_tween().tween_property(backpack_items, "position:x", -570.555, 0.5).set_ease(Tween.EASE_IN_OUT)
 	create_tween().tween_property(backpack_toggle, "position:x", 0, 0.2).set_ease(Tween.EASE_IN_OUT)
 	create_tween().tween_property(cats, "position:x", 0, 0.5).set_ease(Tween.EASE_IN_OUT) 
 	backpack_toggle.disabled = false
-	close_backpack_area.mouse_filter = Control.MOUSE_FILTER_IGNORE
 
 func _on_backpack_open_pressed() -> void:
 	create_tween().tween_property(backpack_toggle, "position:x", -108.91, 0.2).set_ease(Tween.EASE_IN_OUT)
 	backpack_show()
-	
-func _on_close_backpack_area_mouse_entered() -> void:
-	hovering_outside = true
 
-func _on_close_backpack_area_mouse_exited() -> void:
-	hovering_outside = false
+func _on_backpack_close_pressed() -> void:
+	backpack_hide()
